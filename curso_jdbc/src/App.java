534ab37b516e0a26be7d7c58789beee600211c1a
LuccaL <106239988+AlephLLTR@users.noreply.github.com>
2025-05-30 16:50:39 -0300

    Aula de Recuperação de Dados

--- curso_jdbc/src/App.java
+++ curso_jdbc/src/App.java
@@ -1,5 +1,29 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("SELECT * FROM department");
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        } catch (SQLException err) {
+            err.printStackTrace();
+        } finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
     }
 }
